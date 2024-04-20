71caeebc8c365e8ab7c22729ad9723cf9b7f78c5
y-u-y-a <y-u-y-a@users.noreply.github.com>
2024-04-20 15:24:54 +0900

    Delete actions.ts

--- src/app/(home)/actions.ts
+++
@@ -1,9 +0,0 @@
-"use server"
-
-import type { Pokemon } from "@/models/pokemon"
-import { pokemonService } from "@/services/pokemonService"
-
-export async function fetchPokemonsAction(page: number): Promise<Pokemon[]> {
-  const pokemons = await pokemonService.getAll(page)
-  return pokemons
-}
