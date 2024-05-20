611952d032424381123e57f27a9a22be69740934
y-u-y-a <y-u-y-a@users.noreply.github.com>
2024-05-20 13:17:50 +0900

    tmp: fetch at server

--- src/app/(home)/container.tsx
+++ src/app/(home)/container.tsx
@@ -1,30 +1,20 @@
 "use client"
 
-import { pokemonService } from "@/services/pokemonService"
 import { Group, Pagination } from "@mantine/core"
-import { useRouter, useSearchParams } from "next/navigation"
-import useSWR from "swr"
-import { PokemonList } from "./PokemonList"
+import { usePathname, useRouter } from "next/navigation"
 
-/**
- * クエリパラメータのpageを基準に再レンダリングさせている
- * */
-export function RootContainer() {
+export function RootContainer({ currentPage, totalPage }: { currentPage: number; totalPage: number }) {
   const router = useRouter()
-  const currentPage = Number(useSearchParams().get("page")) || 1
-
-  const { data, isLoading } = useSWR(`/pokemons?page=${currentPage}`, () => pokemonService.getPagination(currentPage))
+  const pathname = usePathname()
 
   const refetch = (newPage: number) => {
-    router.replace(`/?page=${newPage}`)
+    router.replace(`${pathname}?page=${newPage}`)
   }
-
   return (
     <>
       <Group mb={20} justify="flex-end">
-        <Pagination color="teal" total={data?.totalPage || 40} value={currentPage} onChange={refetch} />
+        <Pagination color="teal" total={totalPage} value={currentPage} onChange={refetch} />
       </Group>
-      {!isLoading && data && <PokemonList pokemons={data.pokemons} />}
     </>
   )
 }
--- src/services/api.d.ts
+++ src/services/api.d.ts
@@ -3,7 +3,6 @@
  * Do not make direct changes to the file.
  */
 
-
 export interface paths {
   "/companies": {
     /** 企業情報を全件取得する */
@@ -11,54 +10,54 @@ export interface paths {
       responses: {
         200: {
           content: {
-            "application/json": components["schemas"]["getAllCompaniesResponse"];
-          };
-        };
-        404: components["responses"]["NotFound"];
-        500: components["responses"]["InternalServerError"];
-      };
-    };
-  };
+            "application/json": components["schemas"]["getAllCompaniesResponse"]
+          }
+        }
+        404: components["responses"]["NotFound"]
+        500: components["responses"]["InternalServerError"]
+      }
+    }
+  }
 }
 
-export type webhooks = Record<string, never>;
+export type webhooks = Record<string, never>
 
 export interface components {
   schemas: {
-    getAllCompaniesResponse: components["schemas"]["Company"][];
+    getAllCompaniesResponse: components["schemas"]["Company"][]
     Company: {
       /** ID */
-      id: string;
+      id: string
       /** 企業名 */
-      name: string;
+      name: string
       /** メールアドレス */
-      email: string;
-    };
-  };
+      email: string
+    }
+  }
   responses: {
     NotFound: {
       content: {
         "application/json": {
-          error: string;
-        };
-      };
-    };
+          error: string
+        }
+      }
+    }
     InternalServerError: {
       content: {
         "application/json": {
-          error: string;
-        };
-      };
-    };
-  };
-  parameters: never;
-  requestBodies: never;
-  headers: never;
-  pathItems: never;
+          error: string
+        }
+      }
+    }
+  }
+  parameters: never
+  requestBodies: never
+  headers: never
+  pathItems: never
 }
 
-export type $defs = Record<string, never>;
+export type $defs = Record<string, never>
 
-export type external = Record<string, never>;
+export type external = Record<string, never>
 
-export type operations = Record<string, never>;
+export type operations = Record<string, never>
