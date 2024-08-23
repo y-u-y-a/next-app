2e2b5660498ebbc840d963b150cbb321087a1974
y-u-y-a <y-u-y-a@users.noreply.github.com>
2024-08-23 11:16:52 +0900

    fix:

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,5 +1,6 @@
 import { Container } from "@mantine/core"
 import type { Metadata } from "next/types"
+import { CreateUserForm } from "./CreateUserForm"
 
 export const metadata: Metadata = {
   title: "トップ",
@@ -7,5 +8,9 @@ export const metadata: Metadata = {
 }
 
 export default function RootPage() {
-  return <Container py={40}>トップ</Container>
+  return (
+    <Container py={40}>
+      <CreateUserForm mt={40} mx="auto" maw={600} />
+    </Container>
+  )
 }
