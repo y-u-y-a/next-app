5bf7a35b5bd1df9c71b2055d5108f36c517fab1c
y-u-y-a <y-u-y-a@users.noreply.github.com>
2024-09-21 16:35:05 +0900

    Delete template.tsx

--- src/app/template.tsx
+++
@@ -1,9 +0,0 @@
-import { Box } from "@mantine/core"
-
-/**
- * template.tsx -> layout.tsx -> page.tsx
- */
-export default function Template({ children }: { children: React.ReactNode }) {
-  console.log("ページビューのログ記録などに使用します")
-  return <Box>{children}</Box>
-}
